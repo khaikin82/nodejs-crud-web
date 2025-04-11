8ba3242dd5b43a2a56dcdcfb6726a8b88e347fe3
khaihd <hoangdangkhai.dev@gmail.com>
2025-04-11 07:39:30 +0700

    feat: add create table if not exists

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "10.128.0.9",
+  host: "34.173.83.207",
   user: "cruduser",
   password: "crudpass",
   database: "node_crud",
@@ -11,5 +11,19 @@ connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to MySQL!");
 });
+
+connection.query(
+  `
+  CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+  )
+`,
+  (err) => {
+    if (err) throw err;
+    console.log("Table users is ready.");
+  }
+);
 
 module.exports = connection;
